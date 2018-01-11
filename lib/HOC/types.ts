a96575112e09fd0fa54766d6530ec73a75871997
Aviv Rozenboim <aviv@soluto.com>
2018-01-11 17:00:35 +0200

    Re-written withAnalyticOnMount as a plain component, added tests
    Removed sinon
    Changed some naming

--- lib/HOC/types.ts
+++ lib/HOC/types.ts
@@ -1,6 +1,6 @@
 import { AnalyticsDispatcher } from 'shisell';
 
 export type TransformAnalyticsFunc = (dispatcher: AnalyticsDispatcher, otherProps: any) => AnalyticsDispatcher;
-export type propsToExtras = (props: object) => object;
+export type TransformPropsFunc = (props: {}) => {};
 export type predicate = (obj: any) => boolean;
 export type propsWithAnalytics = { analytics: { dispatcher: AnalyticsDispatcher } }
---
+++ lib/HOC/with-analytic-on-mount.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {Requireable} from 'prop-types';
+
+import {TransformPropsFunc, predicate, propsWithAnalytics} from './types';
+import analyticsContextTypes, {AnalyticsContext} from './analytics-context-types';
+
+const defaultPropsToExtrasMapper = () => ({});
+
+export const withAnalyticOnMount = (
+    analyticName: string,
+    mapPropsToExtras: TransformPropsFunc = defaultPropsToExtrasMapper
+) => <P extends {}>(InnerComponent: React.ComponentClass<P> | React.StatelessComponent<P>) =>
+    class WithAnalyticOnMount extends React.Component<P> {
+        context: AnalyticsContext;
+
+        static contextTypes = analyticsContextTypes;
+
+        componentDidMount() {
+            this.context.analytics.dispatcher.withExtras(mapPropsToExtras(this.props)).dispatch(analyticName);
+        }
+
+        render() {
+            return <InnerComponent {...this.props} />;
+        }
+    };
