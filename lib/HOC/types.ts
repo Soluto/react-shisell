851fd246fd3a2770ec575880b91e16afb3318792
Aviv Rozenboim <aviv@soluto.com>
2018-01-14 11:24:10 +0200

    Refactored withOnPropChangedAnalytic
    * Added tests
    * Fixed behaviour to only call predicate if value actually changed
    * Merged two predicates into one

--- lib/HOC/types.ts
+++ lib/HOC/types.ts
@@ -3,4 +3,5 @@ import { AnalyticsDispatcher } from 'shisell';
 export type TransformAnalyticsFunc = (dispatcher: AnalyticsDispatcher, otherProps: any) => AnalyticsDispatcher;
 export type TransformPropsFunc = (props: {}) => {};
 export type predicate = (obj: any) => boolean;
+export type Predicate2 = (val1: any, val2: any) => boolean;
 export type propsWithAnalytics = { analytics: { dispatcher: AnalyticsDispatcher } }
