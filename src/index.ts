4ca02042275d515170782cf0165fad4092509dc4
Aviv Rozenboim <aviv@soluto.com>
2018-01-16 17:25:09 +0200

    Added rollup bundling
    Stopped using the recompose/* imports since rollup does tree shaking

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export {withAnalytics} from './HOC/with-analytics';
-export {withoutAnalytics} from './HOC/without-analytics';
-export {enrichAnalytics} from './HOC/enrich-analytics';
-export {withAnalyticOnEvent} from './HOC/with-analytic-on-event';
-export {withAnalyticOnView} from './HOC/with-analytic-on-view';
-export {withOnPropChangedAnalytic} from './HOC/with-on-prop-changed-analytic';
+export {withAnalytics} from './hoc/with-analytics';
+export {withoutAnalytics} from './hoc/without-analytics';
+export {enrichAnalytics} from './hoc/enrich-analytics';
+export {withAnalyticOnEvent} from './hoc/with-analytic-on-event';
+export {withAnalyticOnView} from './hoc/with-analytic-on-view';
+export {withOnPropChangedAnalytic} from './hoc/with-on-prop-changed-analytic';
 export {default as analytics} from './analytics';
