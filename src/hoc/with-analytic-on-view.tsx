540aeafda2de3325b49085b33692aa1099a22467
Aviv Rozenboim <aviv@soluto.com>
2018-01-22 11:42:35 +0200

    Changed withAnalyticOnMount to withAnalyticOnView
    * Now supports a predicate to send an analytic only after a specific prop event happens

---
+++ src/hoc/with-analytic-on-view.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import {wrapDisplayName} from 'recompose';
+import {Requireable} from 'prop-types';
+
+import analyticsContextTypes, {AnalyticsContext} from '../analytics-context-types';
+
+export type TransformPropsFunc<In, Out> = (props: In) => Out;
+export interface WithAnalyticOnViewConfiguration<T> {
+    analyticName: string;
+    predicate: (props: T) => boolean;
+    mapPropsToExtras: TransformPropsFunc<T, object>;
+}
+
+const defaultPropsToExtrasMapper = () => ({});
+const defaultPredicate = () => true;
+
+export const withAnalyticOnView = <TProps extends object>({
+    analyticName,
+    predicate = defaultPredicate,
+    mapPropsToExtras = defaultPropsToExtrasMapper,
+}: WithAnalyticOnViewConfiguration<TProps>) => (BaseComponent: React.ComponentType<TProps>) =>
+    class WithAnalyticOnView extends React.Component<TProps> {
+        context: AnalyticsContext;
+
+        static contextTypes = analyticsContextTypes;
+        static displayName = wrapDisplayName(BaseComponent, WithAnalyticOnView.name);
+
+        didSendAnalytic = false;
+
+        trySendAnalytic() {
+            if (this.didSendAnalytic || !predicate(this.props)) return;
+
+            this.context.analytics.dispatcher.withExtras(mapPropsToExtras(this.props)).dispatch(analyticName);
+            this.didSendAnalytic = true;
+        }
+
+        componentDidMount() {
+            this.trySendAnalytic();
+        }
+
+        componentDidUpdate() {
+            this.trySendAnalytic();
+        }
+
+        render() {
+            return <BaseComponent {...this.props} />;
+        }
+    };
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export {withAnalytics} from './HOC/with-analytics';
 export {withoutAnalytics} from './HOC/without-analytics';
 export {enrichAnalytics} from './HOC/enrich-analytics';
 export {withAnalyticOnEvent} from './HOC/with-analytic-on-event';
-export {withAnalyticOnMount} from './HOC/with-analytic-on-mount';
+export {withAnalyticOnView} from './HOC/with-analytic-on-view';
 export {withOnPropChangedAnalytic} from './HOC/with-on-prop-changed-analytic';
 export {default as analytics} from './analytics';
