9b7389d295588a093427c14d0f8520adafb785f1
Natalya Shrits <natalya.shrits@asurion.com>
2021-04-20 15:03:30 -0500

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-export {withAnalytics, WithAnalyticsProps} from './hoc/with-analytics';
-export {enrichAnalytics} from './hoc/enrich-analytics';
-export {
-    withAnalyticOnEvent,
-    WithAnalyticOnEventProps,
-    ExtraAnalyticsDataProvider,
-    WithAnalyticOnEventConfiguration,
-} from './hoc/with-analytic-on-event';
-export {withAnalyticOnView, WithAnalyticOnViewConfiguration} from './hoc/with-analytic-on-view';
-export {withOnPropChangedAnalytic, WithOnPropsChangedConfiguration} from './hoc/with-on-prop-changed-analytic';
-export {Analytics, AnalyticsConsumer} from './shisell-context';
-export {useAnalytics} from './hooks/use-analytics';
-export {useAnalyticCallback} from './hooks/use-analytic-callback';
-export {useEventAnalytic} from './hooks/use-event-analytic';
-export {AnalyticsProvider, AnalyticsProviderProps} from './analytics-provider';
+export * from './hoc/with-analytics';
+export * from './hoc/enrich-analytics';
+export * from './hoc/with-analytic-on-event';
+export * from './hoc/with-analytic-on-view';
+export * from './hoc/with-on-prop-changed-analytic';
+
+export * from './hooks/use-analytics';
+
+export * from './hooks/use-analytic-callback';
+export * from './hooks/use-event-analytic';
+
 export {default as analytics} from './analytics';
+export {Analytics, AnalyticsConsumer} from './shisell-context';
+export * from './analytics-provider';
